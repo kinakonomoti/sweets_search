a2d7dc4cd7adbd40f4bc173afc8e138bee78d782
EC2 Default User <ec2-user@ip-172-31-32-41.ap-northeast-1.compute.internal>
2025-01-05 06:36:22 +0000

    マイページ編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to post_path
+    redirect_to post_path(@post)
   end
 
   def index
@@ -15,6 +15,13 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy  
+    redirect_to posts_path
   end
 
   def edit
